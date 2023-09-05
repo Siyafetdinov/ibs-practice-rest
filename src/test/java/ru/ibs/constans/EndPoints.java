73bb581b28eac0d847b6c961dfbdb976e750003b
Siyafetdinov <siyafetdinov@gmail.com>
2023-09-05 23:07:49 +0400

    fix

--- src/test/java/ru/ibs/constans/EndPoints.java
+++ src/test/java/ru/ibs/constans/EndPoints.java
@@ -3,5 +3,5 @@ package ru.ibs.constans;
 public class EndPoints {
     public static final String URL = "http://localhost:8080";
     public static final String API_FOOD = "/api/food";
-    public static final String API_DATA_RESET = "api/data/reset";
+    public static final String API_DATA_RESET = "/api/data/reset";
 }
